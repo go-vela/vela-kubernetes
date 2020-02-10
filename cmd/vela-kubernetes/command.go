d2866010631d9950478b0792a498bda8b298b836
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-02-10 17:30:21 -0600

    feat: add plugin shell content

---
+++ cmd/vela-kubernetes/command.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+const kubectlBin = "/bin/kubectl"
+
+// execCmd is a helper function to
+// run the provided command.
+func execCmd(e *exec.Cmd) error {
+	logrus.Tracef("executing cmd %s", strings.Join(e.Args, " "))
+
+	// set command stdout to OS stdout
+	e.Stdout = os.Stdout
+	// set command stderr to OS stderr
+	e.Stderr = os.Stderr
+
+	// output "trace" string for command
+	fmt.Println("$", strings.Join(e.Args, " "))
+
+	return e.Run()
+}
---
+++ cmd/vela-kubernetes/plugin_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"os/exec"
+	"reflect"
+	"testing"
+)
+
+func TestKubernetes_Plugin_Command(t *testing.T) {
+	// setup types
+	p := &Plugin{}
+
+	want := exec.Command(
+		kubectlBin,
+	)
+
+	// run test
+	got := p.Command()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Command is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_Plugin_Exec(t *testing.T) {
+	// setup types
+	p := &Plugin{}
+
+	err := p.Exec()
+	if err != nil {
+		t.Errorf("Exec returned err: %v", err)
+	}
+}
+
+func TestKubernetes_Plugin_Validate(t *testing.T) {
+	// setup types
+	p := &Plugin{}
+
+	err := p.Validate()
+	if err != nil {
+		t.Errorf("Validate returned err: %v", err)
+	}
+}
