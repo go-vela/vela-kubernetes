3a7165f999dbaee8c31cfb1fcce8613ac9c47be5
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-02-11 10:37:44 -0600

    feat: add cmds for kubectl

--- cmd/vela-kubernetes/command_test.go
+++ cmd/vela-kubernetes/command_test.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"os/exec"
+	"reflect"
 	"testing"
 )
 
@@ -16,5 +17,109 @@ func TestKubernetes_execCmd(t *testing.T) {
 	err := execCmd(e)
 	if err != nil {
 		t.Errorf("execCmd returned err: %v", err)
+	}
+}
+
+func TestKubernetes_applyCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"apply",
+		"--output",
+		"json",
+		"--filename",
+		"apply.yml",
+	)
+
+	got := applyCmd("apply.yml")
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("applyCmd is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_patchCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"patch",
+		"--output",
+		"json",
+		"--filename",
+		"patch.yml",
+	)
+
+	got := patchCmd("patch.yml")
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("patchCmd is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_replaceCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"replace",
+		"--output",
+		"json",
+		"--filename",
+		"replace.yml",
+	)
+
+	got := replaceCmd("replace.yml")
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("replaceCmd is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_statusWatchCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"rollout",
+		"status",
+		"--watch",
+		"true",
+		"myStatus",
+	)
+
+	got := statusWatchCmd("myStatus")
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("statusWatchCmd is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_statusNoWatchCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"rollout",
+		"status",
+		"--watch",
+		"false",
+		"myStatus",
+	)
+
+	got := statusNoWatchCmd("myStatus")
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("statusNoWatchCmd is %v, want %v", got, want)
+	}
+}
+
+func TestKubernetes_versionCmd(t *testing.T) {
+	// setup types
+	want := exec.Command(
+		kubectlBin,
+		"version",
+	)
+
+	got := versionCmd()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("versionCmd is %v, want %v", got, want)
 	}
 }
