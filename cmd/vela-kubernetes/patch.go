9fd50e48db93873a66d2029755d2410b45cdeec8
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-02-12 11:37:05 -0600

    feat(patch): add command method

--- cmd/vela-kubernetes/patch.go
+++ cmd/vela-kubernetes/patch.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/sirupsen/logrus"
 )
@@ -14,6 +15,35 @@ import (
 type Patch struct {
 	// container images from files to patch
 	Images []string
+}
+
+// Command formats and outputs the Patch command from the
+// provided configuration to patch resources.
+func (p *Patch) Command(c *Config) *exec.Cmd {
+	logrus.Trace("creating kubectl patch command from plugin configuration")
+
+	// variable to store flags for command
+	var flags []string
+
+	// check if config namespace is provided
+	if len(c.Namespace) > 0 {
+		// add flag for namespace from provided config namespace
+		flags = append(flags, fmt.Sprintf("--namespace=%s", c.Namespace))
+	}
+
+	// check if config context is provided
+	if len(c.Context) > 0 {
+		// add flag for context from provided config context
+		flags = append(flags, fmt.Sprintf("--context=%s", c.Context))
+	}
+
+	// add flag for apply kubectl command
+	flags = append(flags, "patch")
+
+	// add flag for output
+	flags = append(flags, "--output=json")
+
+	return exec.Command(kubectlBin, flags...)
 }
 
 // Validate verifies the Patch is properly configured.
