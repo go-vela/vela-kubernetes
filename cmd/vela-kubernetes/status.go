bdc2ab41a4fc8a545738f26e33dca6a71415ff49
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-02-12 11:37:15 -0600

    feat(status): add command method

--- cmd/vela-kubernetes/status.go
+++ cmd/vela-kubernetes/status.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -17,6 +18,47 @@ type Status struct {
 	Resources []string
 	// total time allowed to watch Kubernetes resources
 	Timeout time.Duration
+}
+
+// Command formats and outputs the Status command from the
+// provided configuration to watch the status on resources.
+func (s *Status) Command(c *Config, resource string) *exec.Cmd {
+	logrus.Trace("creating kubectl status command from plugin configuration")
+
+	// variable to store flags for command
+	var flags []string
+
+	// check if config namespace is provided
+	if len(c.Namespace) > 0 {
+		// add flag for namespace from provided config namespace
+		flags = append(flags, fmt.Sprintf("--namespace=%s", c.Namespace))
+	}
+
+	// check if config context is provided
+	if len(c.Context) > 0 {
+		// add flag for context from provided config context
+		flags = append(flags, fmt.Sprintf("--context=%s", c.Context))
+	}
+
+	// add flag for status kubectl command
+	flags = append(flags, "rollout", "status")
+
+	// check if resource is provided
+	if len(resource) > 0 {
+		// add flag for resource from provided status resource
+		flags = append(flags, resource)
+	}
+
+	// check if status timeout is provided
+	if s.Timeout > 0 {
+		// add flag for timeout from provided status timeout
+		flags = append(flags, fmt.Sprintf("--timeout=%v", s.Timeout))
+	}
+
+	// add flag for watching status of rollout until it finishes
+	flags = append(flags, "--watch=true")
+
+	return exec.Command(kubectlBin, flags...)
 }
 
 // Validate verifies the Status is properly configured.
--- cmd/vela-kubernetes/status_test.go
+++ cmd/vela-kubernetes/status_test.go
@@ -5,9 +5,45 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+	"reflect"
 	"testing"
 	"time"
 )
+
+func TestKubernetes_Status_Command(t *testing.T) {
+	// setup types
+	c := &Config{
+		File:      "file",
+		Context:   "context",
+		Namespace: "namespace",
+	}
+
+	s := &Status{
+		Resources: []string{"resources"},
+		Timeout:   5 * time.Minute,
+	}
+
+	for _, resource := range s.Resources {
+		want := exec.Command(
+			kubectlBin,
+			fmt.Sprintf("--namespace=%s", c.Namespace),
+			fmt.Sprintf("--context=%s", c.Context),
+			"rollout",
+			"status",
+			resource,
+			fmt.Sprintf("--timeout=%v", s.Timeout),
+			"--watch=true",
+		)
+
+		got := s.Command(c, resource)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Command is %v, want %v", got, want)
+		}
+	}
+}
 
 func TestKubernetes_Status_Validate(t *testing.T) {
 	// setup types
