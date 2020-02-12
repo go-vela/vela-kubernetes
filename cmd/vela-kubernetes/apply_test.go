43f8c2d1ed8064ba299a398e2dcd12044555646f
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-02-12 11:36:46 -0600

    feat(apply): add command method

--- cmd/vela-kubernetes/apply_test.go
+++ cmd/vela-kubernetes/apply_test.go
@@ -5,13 +5,46 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+	"reflect"
 	"testing"
 )
+
+func TestKubernetes_Apply_Command(t *testing.T) {
+	// setup types
+	c := &Config{
+		File:      "file",
+		Context:   "context",
+		Namespace: "namespace",
+	}
+
+	a := &Apply{
+		Files: []string{"apply.yml"},
+	}
+
+	for _, file := range a.Files {
+		want := exec.Command(
+			kubectlBin,
+			fmt.Sprintf("--namespace=%s", c.Namespace),
+			fmt.Sprintf("--context=%s", c.Context),
+			"apply",
+			fmt.Sprintf("--filename=%s", file),
+			"--output=json",
+		)
+
+		got := a.Command(c, file)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Command is %v, want %v", got, want)
+		}
+	}
+}
 
 func TestKubernetes_Apply_Validate(t *testing.T) {
 	// setup types
 	a := &Apply{
-		Files: []string{"files"},
+		Files: []string{"apply.yml"},
 	}
 
 	err := a.Validate()
