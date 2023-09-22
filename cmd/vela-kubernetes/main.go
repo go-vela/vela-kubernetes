929f1eee85854e3334e9d649d3af105d84d5d90f
wass3rw3rk <49894298+wass3rw3rk@users.noreply.github.com>
2023-09-22 11:05:21 -0500

    chore(license): update source code headers + copyright year

--- cmd/vela-kubernetes/main.go
+++ cmd/vela-kubernetes/main.go
@@ -1,6 +1,4 @@
-// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
-//
-// Use of this source code is governed by the LICENSE file in this repository.
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -39,7 +37,7 @@ func main() {
 	app.Name = "vela-kubernetes"
 	app.HelpName = "vela-kubernetes"
 	app.Usage = "Vela Kubernetes plugin for managing Kubernetes resources"
-	app.Copyright = "Copyright (c) 2023 Target Brands, Inc. All rights reserved."
+	app.Copyright = "Copyright 2020 Target Brands, Inc. All rights reserved."
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Vela Admins",
